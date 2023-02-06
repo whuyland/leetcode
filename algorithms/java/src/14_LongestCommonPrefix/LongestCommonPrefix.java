2615d691fb579328122c71e1906134512814724c
whuy <whuyland@gmail.com>
2023-02-06 15:46:10 +0800

    Add solution to 14. Longest Common Prefix

---
+++ algorithms/java/src/14_LongestCommonPrefix/LongestCommonPrefix.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/longest-common-prefix/
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        for (int i = 0; i < strs[0].length(); ++i) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; ++j) {
+                if (i < strs[j].length() && c == strs[j].charAt(i)) {
+                    continue;
+                } else {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
