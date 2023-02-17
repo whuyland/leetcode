f686266fa0eeca3ed371264a1282481ad5b85242
whuyland <whuyland@gmail.com>
2023-02-17 13:48:05 +0800

    Add solution to 53. Maximum Subarray

---
+++ algorithms/java/src/53_MaximumSubarray/MaximumSubarray.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-subarray/
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int num : nums) {
+            if (sum < 0) {
+                sum = num;
+            } else {
+                sum += num;
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
