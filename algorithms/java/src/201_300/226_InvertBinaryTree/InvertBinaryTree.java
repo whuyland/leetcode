5682b2ed00ccf14ac3fa5d0f3672efd45e929ddf
whuyland <whuyland@gmail.com>
2023-03-10 11:15:50 +0800

    Add solution to 226. Invert Binary Tree

---
+++ algorithms/java/src/201_300/226_InvertBinaryTree/InvertBinaryTree.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/invert-binary-tree/
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(left);
+        return root;
+    }
+}
