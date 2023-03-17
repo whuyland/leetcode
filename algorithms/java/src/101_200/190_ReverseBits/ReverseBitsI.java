94952613d048c6c13900171ba426cdf452fa2d0d
whuyland <whuyland@gmail.com>
2023-03-17 11:48:45 +0800

    Add solution to 190. Reverse Bits

---
+++ algorithms/java/src/101_200/190_ReverseBits/ReverseBitsI.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/reverse-bits/
+public class ReverseBitsI {
+    public int reverseBits(int n) {
+        int l = 1 << 31;
+        int r = 1;
+        int ret = 0;
+        for (int i = 0; i < 16; ++i) {
+            if ((n & l) != 0) {
+                ret |= r;
+            }
+            if ((n & r) != 0) {
+                ret |= l;
+            }
+            l = l >>> 1;
+            r = r << 1;
+        }
+        return ret;
+    }
+}
