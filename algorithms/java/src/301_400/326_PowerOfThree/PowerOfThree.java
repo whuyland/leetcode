ca97d1cbc1b667470737ba0f6aea5f5eddbd9f3e
whuyland <whuyland@gmail.com>
2023-03-21 15:56:19 +0800

    Add solution to 326. Power of Three

---
+++ algorithms/java/src/301_400/326_PowerOfThree/PowerOfThree.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/power-of-three/
+public class PowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
