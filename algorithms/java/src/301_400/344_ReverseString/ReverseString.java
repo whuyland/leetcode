c2ce06ab10dc3476d59d176f046c1d2147eb2149
whuyland <whuyland@gmail.com>
2023-03-22 10:42:08 +0800

    Add solution to 344. Reverse String

---
+++ algorithms/java/src/301_400/344_ReverseString/ReverseString.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/reverse-string/
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            char c = s[l];
+            s[l] = s[r];
+            s[r] = c;
+            ++l;
+            --r;
+        }
+    }
+}
