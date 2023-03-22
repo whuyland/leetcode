894354afb12fac1df1b2e55c0f9bfba0d3c0aa3e
whuyland <whuyland@gmail.com>
2023-03-22 18:42:49 +0800

    Add solution to 412. Fizz Buzz

---
+++ algorithms/java/src/401_500/412_FizzBuzz/FizzBuzz.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/fizz-buzz/
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> ret = new ArrayList<>(n);
+        for (int i = 1; i <= n; ++i) {
+            if (i % 15 == 0) {
+                ret.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                ret.add("Fizz");
+            } else if (i % 5 == 0) {
+                ret.add("Buzz");
+            } else {
+                ret.add(String.valueOf(i));
+            }
+        }
+        return ret;
+    }
+}
