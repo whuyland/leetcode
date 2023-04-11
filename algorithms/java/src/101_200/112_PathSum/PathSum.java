1265e931473b14ac3a9297b85b43e1df42efed33
whuyland <whuyland@gmail.com>
2023-04-11 17:19:20 +0800

    Add solution to 112. Path Sum

---
+++ algorithms/java/src/101_200/112_PathSum/PathSum.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/path-sum/
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+    }
+}
